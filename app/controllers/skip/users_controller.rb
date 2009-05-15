ccdea427a23bd978826e1b7648e672e29337f624
moro <moronatural@gmail.com>
2009-05-15 12:00:39 +0900

    ユーザ情報の更新アクションを実装

--- app/controllers/skip/users_controller.rb
+++ app/controllers/skip/users_controller.rb
@@ -1,26 +1,46 @@
 class Skip::UsersController < Skip::ApplicationController
+  before_filter :authenticate_with_oauth, :only => %w[update]
+
   def create
-    # TODO パラメータ化は必要?
-    client = ClientApplication.
-               families.find(:first, :conditions => {:name => Skip::ApplicationController::SKIP_NAME})
-    @user, token = create_user_and_token(client, params[:user])
+    @user, token = ActiveRecord::Base.transaction{ create_user_and_token!(params[:user]) }
     respond_to do |f|
       f.xml{ render :xml => api_response(@user, token).to_xml(:root => "user") }
     end
   rescue ActiveRecord::RecordNotFound => why
-    respond_to do |f|
-      f.xml{ render :xml => @user.errors.to_xml }
+    rendeor_validation_error(current_user)
+  end
+
+  def update
+    current_user.attributes = params[:user]
+    current_user.identity_url = params[:user][:identity_url]
+
+    if current_user.save
+      respond_to do |f|
+        f.xml do
+          token = current_user.tokens.find(:first, :conditions => {:client_application_id => skip,
+                                                                   :type => "AccessToken"})
+          render :xml => api_response(current_user, token).to_xml(:root => "user")
+        end
+      end
+    else
+      rendeor_validation_error(current_user)
     end
   end
 
   private
+  def skip
+    @client ||= ClientApplication.families.find(:first, :conditions => {:name => Skip::ApplicationController::SKIP_NAME})
+  end
 
-  def create_user_and_token(client, user_param)
-    user = User.new(user_param){|u| u.identity_url = user_param[:identity_url] }
-    ActiveRecord::Base.transaction do
-      user.save!
-      return [user, client.publish_access_token(user)]
+  def rendeor_validation_error(model)
+    respond_to do |f|
+      f.xml{ render :xml => model.errors.to_xml, :status => :unprocessable_entity }
     end
+  end
+  def create_user_and_token!(user_param)
+    user = User.new(user_param){|u| u.identity_url = user_param[:identity_url] }
+    user.save!
+    return [user, skip.publish_access_token(user)]
   end
 
   def api_response(user, token)
--- spec/controllers/skip/users_controller_spec.rb
+++ spec/controllers/skip/users_controller_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Skip::UsersController do
+  fixtures :users
   before do
     controller.should_receive(:internal_call_only).and_return true
     @client = ClientApplication.create(:name => "SKIP",
@@ -12,7 +13,6 @@ describe Skip::UsersController do
   describe "POST :create (success)" do
     before do
       post :create, :format => "xml",
-        :client_application_id => @client.id,
         :user => {
           :name => "alice",
           :display_name => "アリス",
@@ -30,6 +30,49 @@ describe Skip::UsersController do
       subject{ Hash.from_xml(response.body)["user"] }
       it{ subject["access_token"].should_not be_blank }
       it{ subject["access_secret"].should_not be_blank }
+    end
+  end
+
+  describe "PUT :update (success)" do
+    before do
+      alice = User.create!(
+          :name => "alice",
+          :display_name => "アリス",
+          :identity_url => "http://op.example.com/u/alice"
+      )
+      @client.publish_access_token(alice)
+
+      controller.should_receive(:authenticate_with_oauth).and_return true
+      controller.send(:current_user=, alice)
+
+      # FIXME Userクラスにラッパつける
+      @old_token = alice.tokens.find(:first, :conditions => {:client_application_id => @client, :type => "AccessToken"})
+
+      put_user
+    end
+
+    def put_user
+      put :update, :format => "xml",
+        :user => {
+          :name => "bob",
+          :display_name => "ボブさん",
+          :identity_url => "http://op.example.com/u/bob",
+        }
+    end
+
+    it("response should be success") { response.should be_success }
+
+    describe "assigned user" do
+      subject{ controller.send(:current_user) }
+      it{ should have(2).tokens }
+      it{ subject.name.should == "bob" }
+      it{ subject.identity_url.should == "http://op.example.com/u/bob" }
+    end
+
+    describe "response" do
+      subject{ Hash.from_xml(response.body)["user"] }
+      it{ subject["access_token"].should == @old_token.token }
+      it{ subject["access_secret"].should == @old_token.secret }
     end
   end
 end
