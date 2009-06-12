57e5fef3c0b330d2fd74e786b62331960545da07
pochi <pochi.black@gmail.com>
2009-06-12 10:47:15 +0900

    [#761] ユーザの一覧画面は参照のみとした

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,7 +2,8 @@ class Admin::ApplicationController < ApplicationController
   before_filter :require_admin
   layout 'admin'
 
-  private
+  # TODO privateにした場合テスト時にメソッドが呼べない
+  # private
   def require_admin
     unless current_user.admin?
       redirect_to root_url
